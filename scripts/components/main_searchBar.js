4d5e9b66a2feaa787b6faf39ad23dee1a9887678
mohammediche <mohammediche@gmail.com>
2023-06-14 22:29:54 +0200

    realisation du filtre de la barre de recherche principal, selon name, description, et ingredient

---
+++ scripts/components/main_searchBar.js
@@ -0,0 +1,30 @@
+const mainSearchBar = document.getElementById("main_searchBar");
+
+const main_searchBarFunc = (allDataCardsRecette) => {
+  mainSearchBar.addEventListener("input", (e) => {
+    const valueSearch = e.target.value;
+    const valuePrevLS = localStorage.getItem("valuePrev");
+    // on stock la valeur précédente pour vérifier le else If
+    localStorage.setItem("valuePrev", valueSearch);
+    if (valueSearch.trim().length >= 3) {
+      const filteredBySearch = allDataCardsRecette?.filter((item) => {
+        return Object.entries(item).some(([keys, value]) => {
+          if (keys === "name" || keys === "description") {
+            return value.toString().toLowerCase().includes(valueSearch.toLowerCase());
+          }
+          if (keys === "ingredients" && Array.isArray(value)) {
+            console.log("ok c'est rentré dans ingredients");
+            return value.some((ingredient) => {
+              return ingredient.ingredient.toString().toLowerCase().includes(valueSearch.toLowerCase());
+            });
+          }
+        });
+        // return Object.values(item).some((value) => value.toString().toLowerCase().includes(valueSearch.toLowerCase()));
+      });
+      console.log("filteredBySearch=====>", filteredBySearch);
+      displayCardRecetteData(filteredBySearch);
+    } else if (valuePrevLS && valuePrevLS.trim().length >= 3) {
+      displayCardRecetteData(allDataCardsRecette);
+    }
+  });
+};
