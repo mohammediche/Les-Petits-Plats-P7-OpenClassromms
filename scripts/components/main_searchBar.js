f5cd6c3c282f8ddf9b5eb0583e550ef66393ea08
mohammediche <mohammediche@gmail.com>
2023-09-02 17:21:16 +0200

    mise en place de la fonction main_searchBarFunc() en boucle native

--- scripts/components/main_searchBar.js
+++ scripts/components/main_searchBar.js
@@ -1,29 +1,47 @@
 const mainSearchBar = document.getElementById("main_searchBar");
 
 const main_searchBarFunc = (allDataCardsRecette) => {
-  mainSearchBar.addEventListener("input", (e) => {
+  mainSearchBar.addEventListener("input", function (e) {
     const valueSearch = e.target.value;
     const valuePrevLS = localStorage.getItem("valuePrev");
-    // on stock la valeur précédente pour vérifier le else If
+
+    // Stocker la valeur précédente pour vérifier le else If
     localStorage.setItem("valuePrev", valueSearch);
+
     if (valueSearch.trim().length >= 3) {
-      const filteredBySearch = allDataCardsRecette?.filter((item) => {
-        return Object.entries(item).some(([keys, value]) => {
-          if (keys === "name" || keys === "description") {
-            return value.toString().toLowerCase().includes(valueSearch.toLowerCase());
+      const filteredBySearch = [];
+
+      for (let i = 0; i < allDataCardsRecette.length; i++) {
+        const item = allDataCardsRecette[i];
+        let match = false;
+
+        for (const [key, value] of Object.entries(item)) {
+          if (
+            (key === "name" || key === "description") &&
+            value.toString().toLowerCase().includes(valueSearch.toLowerCase())
+          ) {
+            match = true;
+            break;
           }
-          if (keys === "ingredients" && Array.isArray(value)) {
-            return value.some((ingredient) => {
-              return ingredient.ingredient.toString().toLowerCase().includes(valueSearch.toLowerCase());
-            });
+
+          if (key === "ingredients" && Array.isArray(value)) {
+            for (const ingredient of value) {
+              if (ingredient.ingredient.toString().toLowerCase().includes(valueSearch.toLowerCase())) {
+                match = true;
+                break;
+              }
+            }
           }
-        });
-        // return Object.values(item).some((value) => value.toString().toLowerCase().includes(valueSearch.toLowerCase()));
-      });
-      // On supprime nos tags au saisi de la main search bar
+        }
+
+        if (match) {
+          filteredBySearch.push(item);
+        }
+      }
+
+      // Supprimer les tags au saisi de la barre de recherche principale
       localStorage.removeItem("selectedTagsLS");
       initFilterRecipes_ByLocalStorageTags(filteredBySearch);
-      /**/
       displayCardRecetteData(filteredBySearch, "search");
     } else if (valuePrevLS && valuePrevLS.trim().length >= 3) {
       displayCardRecetteData(allDataCardsRecette, "search");
